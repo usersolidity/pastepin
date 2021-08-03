ad54539fc537b307fb5a3de6dca38d3fbbdaa76f
0xMartian <87619808+0xMartian@users.noreply.github.com>
2021-08-03 13:45:09 +0200

    store tiptap html using web3.storage

--- components/Tiptap.tsx
+++ components/Tiptap.tsx
@@ -1,10 +1,34 @@
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import { useCallback, useEffect } from 'react'
+import { Web3Storage } from 'web3.storage'
+
+const client = new Web3Storage({
+  token: process.env.NEXT_PUBLIC_WEB3_TOKEN as string,
+})
 
 const Tiptap = () => {
   const editor = useEditor({
     extensions: [StarterKit],
     content: '<p>Hello World! 🌎️</p>',
+  })
+
+  const submit = useCallback(
+    async (e: KeyboardEvent) => {
+      if (e.metaKey && e.key === 'Enter' && editor) {
+        const html = editor.getHTML()
+        const blob = new Blob([html], { type: 'text/html' })
+        console.log('storing')
+        const cid = await client.put([new File([blob], 'title.html')])
+        console.log('stored files with cid:', cid)
+      }
+    },
+    [editor],
+  )
+
+  useEffect(() => {
+    document.addEventListener('keydown', submit)
+    return () => document.removeEventListener('keydown', submit)
   })
 
   return <EditorContent editor={editor} />
